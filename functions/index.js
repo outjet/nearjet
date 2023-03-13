ada11a6d102447157d5418ee08558d1a4e6bbb54
outjet <git@outjet.com>
2023-03-12 23:32:50 -0400

    Moved secret

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,46 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
 const axios = require('axios');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const client = new SecretManagerServiceClient();
 
-exports.myFunction = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const swLat = req.query.swLat;
-    const swLon = req.query.swLon;
-    const neLat = req.query.neLat;
-    const neLon = req.query.neLon;
-    // const apiUrl = `https://aeroapi.flightaware.com/aeroapi/flights/search?query=-latlong "${swLat} ${swLon} ${neLat} ${neLon}"`;
-    const apiUrl = `https://aeroapi.flightaware.com/aeroapi/flights/search/advanced?query={true inAir} {range lat ${swLat} ${neLat}} {range lon ${swLon} ${neLon}}`;
-    axios.get(apiUrl, {
-      headers: {
-        'x-apikey': 'L9oe2sg40TFLeWzSIKsvcyKffuu0wJBB'
-      }
-    })
-      .then(response => {
-        res.set('Access-Control-Allow-Origin', '*');
-        res.set('Access-Control-Allow-Methods', 'GET, POST');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        console.log(res.getHeaders()); // log the response headers
-
-        res.status(200).json(response.data);
+exports.myFunction = functions.https.onRequest(async (req, res) => {
+  try {
+    const version = await client.accessSecretVersion({
+      name: 'projects/134243935912/secrets/aeroapi',
+    });
+    const ohgoApiKey = version.payload.data.toString();
+    cors(req, res, () => {
+      const swLat = req.query.swLat;
+      const swLon = req.query.swLon;
+      const neLat = req.query.neLat;
+      const neLon = req.query.neLon;
+      // const apiUrl = `https://aeroapi.flightaware.com/aeroapi/flights/search?query=-latlong "${swLat} ${swLon} ${neLat} ${neLon}"`;
+      const apiUrl = `https://aeroapi.flightaware.com/aeroapi/flights/search/advanced?query={true inAir} {range lat ${swLat} ${neLat}} {range lon ${swLon} ${neLon}}`;
+      axios.get(apiUrl, {
+        headers: {
+          'x-apikey': `${aeroapi}`
+        }
       })
-      .catch(error => {
-        res.set('Access-Control-Allow-Origin', '*');
-        res.set('Access-Control-Allow-Methods', 'GET, POST');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        console.log(res.getHeaders()); // log the response headers
+        .then(response => {
+          res.set('Access-Control-Allow-Origin', '*');
+          res.set('Access-Control-Allow-Methods', 'GET, POST');
+          res.set('Access-Control-Allow-Headers', 'Content-Type');
+          console.log(res.getHeaders()); // log the response headers
 
-        res.status(500).json({ error: error });
-      });
-  });
+          res.status(200).json(response.data);
+        })
+        .catch(error => {
+          res.set('Access-Control-Allow-Origin', '*');
+          res.set('Access-Control-Allow-Methods', 'GET, POST');
+          res.set('Access-Control-Allow-Headers', 'Content-Type');
+          console.log(res.getHeaders()); // log the response headers
+
+          res.status(500).json({ error: error });
+        });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
 });
